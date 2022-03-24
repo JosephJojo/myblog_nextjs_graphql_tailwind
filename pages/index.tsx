dc9987214b7f5be9edfe719187b212a2555b2bcb
JosephJojo <josephjojojohn@gmail.com>
2022-03-25 01:04:21 +0530

    Carousel - Featured Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { getPosts } from '../services';
 
 import { PostCard, Categories, PostWidget } from '../components'
+import { FeaturedPosts } from '../sections';
 
 // const posts = [
 //   { title: 'Sample Title 01', excerpt: 'Sample Excerpt 01' },
@@ -16,6 +17,7 @@ const Home: NextPage = ({ posts }) => {
         <title>Joseph's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {/* {
